8ed7f4d408367af799182c1b9188a309f37ae37c
tomoya.chihara <ctmajor07@gmail.com>
2021-11-10 14:44:15 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'tasks#index'
   
-    root to: 'tasks#index'
-    
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
     get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :create]
+    resources :users, only: [:index, :show, :new, :create]
     resources :tasks
 end
 
